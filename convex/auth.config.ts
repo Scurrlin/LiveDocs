e5ff657bb0c3d06ea9772126900cad29b7924c74
Sean Currlin <sean.currlin@gmail.com>
2024-11-10 17:36:09 -0500

    add auth config

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://one-scorpion-34.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
