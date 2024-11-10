4cfd118b13a69e46babc96fcd8926da58abc1d5e
Sean Currlin <sean.currlin@gmail.com>
2024-11-10 12:28:48 -0500

    add files.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as files from "../files.js";
 import type * as openai from "../openai.js";
 import type * as podcasts from "../podcasts.js";
 import type * as tasks from "../tasks.js";
@@ -27,6 +28,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  files: typeof files;
   openai: typeof openai;
   podcasts: typeof podcasts;
   tasks: typeof tasks;
