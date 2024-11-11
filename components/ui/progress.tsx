f4066e18e140bed51f64bd0eb1c69ff5dd0f8137
Sean Currlin <sean.currlin@gmail.com>
2024-11-11 15:04:42 -0500

    update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,28 +1,37 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  value: number;
+  max?: number;
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-black-5",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="size-full flex-1 bg-white-1 transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+  ProgressProps
+>(({ className, value, max = 100, ...props }, ref) => {
+  const safeMax = max > 0 ? max : 100;
+  const progressValue = value && safeMax > 0 ? (value / safeMax) * 100 : 0;
 
-export { Progress }
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-4 w-full overflow-hidden rounded-full bg-black-5",
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className="size-full flex-1 bg-white-1 transition-all"
+        style={{ transform: `translateX(-${100 - progressValue}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
