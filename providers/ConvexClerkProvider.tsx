30d0b55e72edfddd462eedf1cfff561d441916b3
Sean Currlin <sean.currlin@gmail.com>
2024-11-11 09:01:06 -0500

    remove dev text on login

--- providers/ConvexClerkProvider.tsx
+++ providers/ConvexClerkProvider.tsx
@@ -9,7 +9,8 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as strin
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
   <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
-    layout: { 
+    layout: {
+      unsafe_disableDevelopmentModeWarnings: true, 
       socialButtonsVariant: 'iconButton',
       logoImageUrl: '/icons/auth-logo.svg'
     },
